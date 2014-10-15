4d10773bfa95661743ea17147ab437390ffec1b0
applede <jksong3@gmail.com>
2014-10-15 22:09:43 +0900

    Initial Commit

---
+++ JaMe/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JaMe
+//
+//  Created by Jake Song on 10/15/14.
+//  Copyright (c) 2014 Jake Song. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ JaMe/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JaMe
+//
+//  Created by Jake Song on 10/15/14.
+//  Copyright (c) 2014 Jake Song. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
