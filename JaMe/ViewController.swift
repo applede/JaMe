e6d33169717734b2ced513837d78313c40d7044d
applede <jksong3@gmail.com>
2014-11-09 20:01:11 +0900

    CALayer로 menuView 실험

--- JaMe/ViewController.swift
+++ JaMe/ViewController.swift
@@ -14,35 +14,9 @@ class ViewController: NSViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
-//    view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[menuView]|", options: nil,
-//                        metrics: nil, views:["menuView": self.menuView]))
-//    view.superview!.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[view]|", options: nil, metrics: nil, views:["view": self.view]))
-//    let menuView = NSView()
-//    view.addSubview(menuView)
-//    menuView.translatesAutoresizingMaskIntoConstraints = false
-//    view.translatesAutoresizingMaskIntoConstraints = false
-
-
-//    view.addConstraint(NSLayoutConstraint(item: menuView, attribute: .Bottom, relatedBy: .Equal,
-//                        toItem: view, attribute: .Bottom, multiplier: 0.8, constant: 0))
     view.addConstraint(NSLayoutConstraint(item: menuView, attribute: .Height, relatedBy: .Equal,
                         toItem: view, attribute: .Height, multiplier: 0.2, constant: 0))
-//    menuView.layer?.backgroundColor = NSColor.whiteColor().CGColor
-//    view.needsLayout = true
-
-    // Do any additional setup after loading the view.
-//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1_000_000_000), dispatch_get_main_queue(), {
-////      self.view.layout()
-//      if let image = NSImage(contentsOfFile: "/Users/apple/hobby/test_jamp/movie/5 Centimeters Per Second (2007)/fanart.jpg") {
-//        let desiredScaleFactor = gwindow?.backingScaleFactor
-//        let actualScaleFactor = image.recommendedLayerContentsScale(desiredScaleFactor!)
-//
-//        self.view.layer?.contents = image.layerContentsForContentsScale(actualScaleFactor)
-//        self.view.layer?.contentsScale = actualScaleFactor
-//        self.menuView.layer?.backgroundColor = NSColor.whiteColor().CGColor
-//
-//      }
-//    })
+    view.layerContentsPlacement = .ScaleProportionallyToFill
   }
 
   override var representedObject: AnyObject? {
@@ -52,7 +26,8 @@ class ViewController: NSViewController {
   }
 
   override func viewDidAppear() {
-    if let image = NSImage(contentsOfFile: "/Users/apple/hobby/test_jamp/movie/5 Centimeters Per Second (2007)/fanart.jpg") {
+//    if let image = NSImage(contentsOfFile: "/Users/apple/hobby/test_jamp/movie/5 Centimeters Per Second (2007)/fanart.jpg") {
+    if let image = NSImage(contentsOfFile: "/Users/apple/hobby/test_jamp/movie/About Time (2013)/fanart.jpg") {
       let desiredScaleFactor = gwindow?.backingScaleFactor
       let actualScaleFactor = image.recommendedLayerContentsScale(desiredScaleFactor!)
 
@@ -60,7 +35,7 @@ class ViewController: NSViewController {
       self.view.layer?.contentsScale = actualScaleFactor
 
     }
-    self.menuView.layer?.backgroundColor = NSColor.whiteColor().CGColor
+    self.menuView.layer?.backgroundColor = NSColor(calibratedWhite: 1.0, alpha: 1.0).CGColor
   }
 
 }
